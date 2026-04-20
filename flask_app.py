7bf49e904b669c388399fe7b6e3039f57c7db1c2
MMNAA <90513561+MMNAA@users.noreply.github.com>
2026-04-20 10:46:05 +0000

    Commited changes.

--- flask_app.py
+++ flask_app.py
@@ -1,16 +1,21 @@
-from flask import Flask, render_template_string, render_template, jsonify, request, redirect, url_for, session
-from flask import render_template
-from flask import json
-from urllib.request import urlopen
-from werkzeug.utils import secure_filename
-import sqlite3
+from flask import Flask, jsonify
+import subprocess
 
 app = Flask(__name__)
 
-@app.get("/")
-def consignes():
-     return render_template('consignes.html')
+@app.route("/")
+def home():
+    return "API Monitoring is running"
 
-if __name__ == "__main__":
-    # utile en local uniquement
-    app.run(host="0.0.0.0", port=5000, debug=True)
+@app.route("/health")
+def health():
+    result = subprocess.run(
+        ["pytest"],
+        capture_output=True,
+        text=True
+    )
+
+    return jsonify({
+        "status": "OK" if result.returncode == 0 else "FAIL",
+        "tests": result.stdout
+    })
